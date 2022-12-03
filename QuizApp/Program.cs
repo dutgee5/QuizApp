aae0bc966a1b95bcc20d429221fff93f14fd6ec5
Coders <muzodone5@gmail.com>
2022-12-03 20:56:28 +0300

    Add project files.

---
+++ QuizApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace QuizApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question q1 = new Question
+                (1,
+                "How many hours a day do you work?",
+                new string[] { "4", "5", "6", "7" },
+                "5");
+
+            Question q2 = new Question
+             (2,
+             "What is your monthly income?",
+             new string[] { "10000$", "15000$", "17500$", "20000$" },
+             "10000$");
+
+
+            Question[] questions = { q1, q2 };
+
+            foreach (var item in questions)
+            {
+                Console.WriteLine(item.QuestionNumber + "-" + item.Text);
+
+                foreach (var item2 in item.Choices)
+                {
+                    Console.WriteLine($"-> {item2}");
+                }
+
+                Console.Write("Answer: ");
+                var cvp = Console.ReadLine();
+                Console.WriteLine(item.checkAnswer(cvp));
+            }
+
+            
+
+            
+            
+        }
+    }
+}
---
+++ QuizApp/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApp
+{
+    class Question
+    {
+        public int QuestionNumber { get; set; }
+        public string Text { get; set; }
+        public string[] Choices { get; set; }
+        public string Answer { get; set; }
+        
+
+
+
+
+        public Question(int questionnmber,string text,string[] choices,string answer)
+        {
+            this.QuestionNumber = questionnmber;
+            this.Text = text;
+            this.Choices = choices;
+            this.Answer = answer;
+        }
+
+
+        public bool checkAnswer(string answer)
+        {
+            return this.Answer.ToLower() == answer.ToLower();
+        }
+    }
+}

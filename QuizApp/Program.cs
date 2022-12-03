37a6bbaf934d77b6dfc95b156a4d084837f6146c
Coders <muzodone5@gmail.com>
2022-12-03 21:43:26 +0300

    sorular güncellendi

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -8,35 +8,21 @@ namespace QuizApp
         {
             Question q1 = new Question
                 (1,
-                "How many hours a day do you work?",
-                new string[] { "4", "5", "6", "7" },
-                "5");
+                "What is a correct syntax to output 'Hello World' in C#?",
+                new string[] { "print ('Hello World');", "Console.WriteLine('Hello World');", "System.out.println('Hello World');", "cout<<'Hello World';" },
+                "Console.WriteLine('Hello World');");
 
             Question q2 = new Question
              (2,
-             "What is your monthly income?",
-             new string[] { "10000$", "15000$", "17500$", "20000$" },
-             "10000$");
+             "How do you create a variable with the numeric value 5?",
+             new string[] { "double x=5;", "int x=5;", "x=5", "num x=5" },
+             "int x=5;");
 
 
             Question[] questions = { q1, q2 };
 
-            foreach (var item in questions)
-            {
-                Console.WriteLine(item.QuestionNumber + "-" + item.Text);
-
-                foreach (var item2 in item.Choices)
-                {
-                    Console.WriteLine($"-> {item2}");
-                }
-
-                Console.Write("Answer: ");
-                var cvp = Console.ReadLine();
-                Console.WriteLine(item.checkAnswer(cvp));
-            }
-
-            
-
+            Quiz quiz = new Quiz(questions);
+            quiz.DisplayQuestion();
             
             
         }
---
+++ QuizApp/Quiz.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApp
+{
+    class Quiz
+    {
+        private Question[] Questions { get; set; }
+        public int QuestionIndex { get; set; }
+
+        public Quiz(Question[] questions)
+        {
+            this.Questions = questions;
+            this.QuestionIndex = 0;
+
+        }
+
+
+        public Question GetQuestion()
+        {
+            return this.Questions[this.QuestionIndex];
+        }
+
+
+        public void Guess(string answer)
+        {
+            var question = this.GetQuestion();
+            Console.WriteLine(question.checkAnswer(answer));
+            this.QuestionIndex++;
+
+            if (this.Questions.Length==this.QuestionIndex)
+            {
+                //skor
+            }
+            else
+                this.DisplayQuestion();
+
+        }
+
+
+        public void DisplayQuestion()
+        {
+            var question = this.GetQuestion();
+            Console.WriteLine($"soru {this.QuestionIndex+1}: {question.Text}");
+
+            foreach (var item2 in question.Choices)
+            {
+                Console.WriteLine($"-> {item2}");
+            }
+
+            Console.Write("Answer: ");
+            var cvp = Console.ReadLine();
+            this.Guess(cvp);
+
+        }
+    }
+}
